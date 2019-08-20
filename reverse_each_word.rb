2ada212e98b795d1da0df18003ad5365750fc4d1
Stephen Blais <stephen@s-blais.com>
2019-08-20 19:48:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word (sentence)
   split_sentence = sentence.split(" ") #makes array
-  output_sentence = "" #creates string for output
+  String.new ('output_sentence') #creates string for output
   split_sentence.each do |word|
     output_sentence.concat "#{word.reverse!}"
     end

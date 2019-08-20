e5cd52df9d143659c93f11acc759cefaa1294631
Stephen Blais <stephen@s-blais.com>
2019-08-20 19:32:44 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word (sentence)
+  split_sentence = sentence.split(" ")
+  output_sentence = ""
+  split_sentence.each do |word|
+    output_sentence.concat ("#{word.reverse}")
+    end
+  puts output_sentence
+end
